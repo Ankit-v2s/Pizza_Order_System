58bb8d8c94eb5bcf62fa9a81dc2f4a0a63a2a3ce
Ankit_v2s <ankit.v2stech@gmail.com>
2022-09-12 15:40:46 +0530

    Sep 12 Push#1

--- pizzaordersystem/src/main/java/com/pizzaordersystem/configuration/ApplicationConfig.java
+++ pizzaordersystem/src/main/java/com/pizzaordersystem/configuration/ApplicationConfig.java
@@ -8,10 +8,9 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class ApplicationConfig implements WebApplicationInitializer{
+public class ApplicationConfig implements WebApplicationInitializer {
 
 	public void onStartup(ServletContext servletContext) throws ServletException {
-
 		AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
 		webApplicationContext.register(ProjectConfig.class);
 		DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
